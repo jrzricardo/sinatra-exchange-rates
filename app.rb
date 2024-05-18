22647ded3e4335e1ae1bb7d9deabb518b628b6ba
Ricardo Juarez <146899774+jrzricardo@users.noreply.github.com>
2024-05-18 19:04:50 +0000

    built dynamic pages

--- app.rb
+++ app.rb
@@ -10,3 +10,29 @@ get("/") do
   
   erb(:homepage)
 end
+
+get("/:from_currency") do
+  @original_currency = params.fetch("from_currency")
+
+  api_url = "https://api.exchangerate.host/list?access_key=#{ENV.fetch("EXCHANGE_RATE_KEY")}"
+  @raw_response = HTTP.get(api_url)
+  @raw_string = @raw_response.to_s
+  @parsed_data = JSON.parse(@raw_string)
+  @currency_list = @parsed_data.fetch("currencies").keys
+
+  erb(:from_to)
+end
+
+get("/:from_currency/:to_currency") do
+    @original_currency = params.fetch("from_currency")
+    @destination_currency = params.fetch("to_currency")
+
+api_url = "https://api.exchangerate.host/convert?access_key=#{ENV.fetch("EXCHANGE_RATE_KEY")}&from=#{@original_currency}&to=#{@destination_currency}&amount=1"
+
+    @raw_response = HTTP.get(api_url)
+    @raw_string = @raw_response.to_s
+    @parsed_data = JSON.parse(@raw_string)
+    @currency_exchange = @parsed_data.fetch("result")
+
+    erb(:exchange)
+end
